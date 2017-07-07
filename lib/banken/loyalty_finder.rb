a311f1519c536b75ed066525c2d3c2e22f3411f9
Yoshiyuki Hirano <yhirano@me.com>
2017-07-08 08:05:26 +0900

    Attribute name to the same name as an argument name

--- lib/banken/loyalty_finder.rb
+++ lib/banken/loyalty_finder.rb
@@ -2,10 +2,10 @@ module Banken
   class LoyaltyFinder
     SUFFIX = "Loyalty"
 
-    attr_reader :controller
+    attr_reader :controller_name
 
-    def initialize(controller)
-      @controller = controller.to_s
+    def initialize(controller_name)
+      @controller_name = controller_name.to_s
     end
 
     def loyalty
@@ -15,13 +15,13 @@ module Banken
     end
 
     def loyalty!
-      loyalty || raise(NotDefinedError, "unable to find loyalty `#{loyalty_name}` for `#{controller}`")
+      loyalty || raise(NotDefinedError, "unable to find loyalty `#{loyalty_name}` for `#{controller_name}`")
     end
 
     private
 
       def loyalty_name
-        "#{controller.camelize}#{SUFFIX}"
+        "#{controller_name.camelize}#{SUFFIX}"
       end
   end
 end
